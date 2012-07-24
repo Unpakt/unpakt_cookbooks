746623195445d8b8e807444b538612ed848371b7
Ian Zabel & Robbie Clutton <pair+izabel+rclutton@pivotallabs.com>
2012-07-24 15:47:04 -0400

    Unpakt specific OSX development recipes

--- unpakt_workstation/recipes/meta_unpakt_lion_image.rb
+++ unpakt_workstation/recipes/meta_unpakt_lion_image.rb
@@ -1,6 +1,6 @@
 # Base recipe for Lion
 include_recipe "pivotal_workstation::meta_osx_base"
-include_recipe "pivotal_workstation::meta_osx_development"
+include_recipe "unpakt_workstation::meta_osx_development"
 include_recipe "unpakt_workstation::meta_unpakt_ruby_development"
 include_recipe "unpakt_workstation::meta_unpakt_specifics"
 include_recipe "pivotal_workstation::function_keys"
