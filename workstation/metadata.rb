8ad949a07ca99ca818fb66d67ef1b2fd8ecd4567
Robbie Clutton <pair+rclutton@pivotallabs.com>
2012-07-24 14:26:31 -0400

    Unpakt specific base recipes.

---
+++ workstation/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Unpakt"
+maintainer_email  "dev@unpakt.com"
+license           "MIT"
+description       "Configure frequently-used tools for an OSX workstation"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+supports          "mac_os_x"
